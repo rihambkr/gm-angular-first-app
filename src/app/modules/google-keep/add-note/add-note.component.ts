8647c58ff2a0d0e2e8578b96d52ca6488632d460
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-04-03 16:31:29 +0100

    add

--- src/app/modules/google-keep/add-note/add-note.component.ts
+++ src/app/modules/google-keep/add-note/add-note.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {KeepService} from '../../../services/keep.service';
 import {Keep} from '../../../models/keep';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-add-note',
@@ -13,7 +14,8 @@ export class AddNoteComponent implements OnInit {
   content = new FormControl();
   keeps: Keep[];
 
-  constructor(private service: KeepService) {
+  constructor(private service: KeepService,
+              private router: Router) {
     this.service.getKeep().subscribe(keeps => {
       this.keeps = keeps;
     }, error => console.log(error));
@@ -26,7 +28,7 @@ export class AddNoteComponent implements OnInit {
     const keep = new Keep(this.title.value, this.content.value);
     this.keeps.push(keep);
     this.service.keep.next(this.keeps);
-    
+    this.router.navigate(['/keeps']);
   };
 
 }
--- src/app/modules/google-keep/card-keep/card-keep.component.ts
+++ src/app/modules/google-keep/card-keep/card-keep.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Keep} from '../../../models/keep';
+import {KeepService} from '../../../services/keep.service';
 
 @Component({
   selector: 'app-card-keep',
@@ -8,11 +9,25 @@ import {Keep} from '../../../models/keep';
 })
 export class CardKeepComponent implements OnInit {
   @Input() keep: Keep;
+  keeps: Keep[];
 
-  constructor() {
+  constructor(private  service: KeepService) {
+    this.service.getKeep().subscribe(res => {
+      console.log(res);
+      this.keeps = res;
+    }, (error: any) => {
+      console.log('ERROR : ' + error);
+    });
   }
 
   ngOnInit(): void {
   }
+
+  remove = (title: string) => {
+    const listTitle = this.keeps.map(e => e.title);
+    const i = listTitle.indexOf(title);
+    this.keeps.splice(i, 1);
+    this.service.keep.next(this.keeps);
+  };
 
 }
